b55e8015559ed59a43547ee9e8c04fd832028f6d
kerupuk99gaming <kerupuk99gaming@gmail.com>
2025-12-06 15:52:23 +0700

    kontak

--- src/components/Blog/blogsContainer.js
+++ src/components/Blog/blogsContainer.js
@@ -63,7 +63,7 @@ const BlogsContainer = ({ data }) => {
           ))}
         </div>
       </Fade>
-      <div className="w-auto p-8 m-4 h-72 sm:h-96 xs:h-96 xxs:h-96 mt-10 bg-gradient-to-r from-yellow to-yellow rounded-xl flex flex-col items-center justify-center">
+      <div className="w-auto p-8 m-4 h-72 sm:h-96 xs:h-96 xxs:h-96 mt-10 bg-yellow bg-gradient-to-r from-yellow to-yellow rounded-xl flex flex-col items-center justify-center">
         <h2 className=" text-3xl font-bold text-black text-center">
           MAU PESAN KERUPUK KULIT SAPI KAMI?
         </h2>
